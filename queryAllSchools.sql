c879f97eeb7b589f38ee5f20c23034139a20d6ed
joanamds <joanamdsantos1@gmail.com>
2023-03-09 11:00:08 -0300

    renomeando os arquivos de criação da tabela e dos dados

--- queryAllSchools.sql
+++
@@ -1,8 +0,0 @@
-SELECT escolas.escola_id, escolas.grupo_id, escolas.nome_escola, escolas.fundacao, cidades.cidade
-FROM EscolasDeSamba.escolas_rj AS escolas
-JOIN EscolasDeSamba.cidades AS cidades ON escolas.cidade_id = cidades.id
-UNION
-SELECT escolas.escola_id, escolas.grupo_id, escolas.nome_escola, escolas.fundacao, cidades.cidade
-FROM EscolasDeSamba.escolas_sp AS escolas
-JOIN EscolasDeSamba.cidades AS cidades ON escolas.cidade_id = cidades.id
-ORDER BY fundacao;
